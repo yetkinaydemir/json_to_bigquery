d200871b6ae36d0a7446a9f516d54c349476dd21
yetkinaydemir <yetkinaydemirr@gmail.com>
2023-01-02 16:27:16 +0300

    Added create_or_update to the main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from logger import log_config
 from google.cloud import bigquery
 from read_data import extract_data
 from create_schema_for_bq import create_bq_schema
+from create_or_update_table import create_or_update
 
 bq_client = bigquery.Client()
 
@@ -11,6 +12,7 @@ file_name = 'challenge_data.json'
 project_id = 'your-project-id'
 dataset_id = 'your-dataset-id'
 table_id = 'your-table-id'
+full_table_name = '{}.{}.{}'.format(project_id, dataset_id, table_id)
 
 if __name__ == '__main__':
 
@@ -19,3 +21,5 @@ if __name__ == '__main__':
     json_list = extract_data(bucket_name, file_name)
 
     bq_schema, source_field_paths = create_bq_schema(json_list)
+
+    create_or_update(full_table_name, bq_schema, source_field_paths)
