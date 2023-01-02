8f6b6f8a7e83c6b897b37911161f723fcf285b4c
yetkinaydemir <yetkinaydemirr@gmail.com>
2023-01-02 16:49:00 +0300

    uri

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from google.cloud import bigquery
 from read_data import extract_data
 from create_schema_for_bq import create_bq_schema
 from create_or_update_table import create_or_update
+from load_data import load_data
 
 bq_client = bigquery.Client()
 
-bucket_name = 'bckt_challenge'
-file_name = 'challenge_data.json'
+bucket_name = 'your-bucket'
+file_name = 'your-file.json'
+uri = 'your-bucket-that-consists-json-file'
 
 project_id = 'your-project-id'
 dataset_id = 'your-dataset-id'
@@ -23,3 +25,4 @@ if __name__ == '__main__':
     bq_schema, source_field_paths = create_bq_schema(json_list)
 
     create_or_update(full_table_name, bq_schema, source_field_paths, project_id, dataset_id, table_id, logger)
+
