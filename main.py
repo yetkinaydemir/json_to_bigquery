345e91313c3d01267f9de73288cce3d7edf2da00
yetkinaydemir <yetkinaydemirr@gmail.com>
2023-01-02 16:24:19 +0300

    Added gcp project info

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ bq_client = bigquery.Client()
 bucket_name = 'bckt_challenge'
 file_name = 'challenge_data.json'
 
+project_id = 'your-project-id'
+dataset_id = 'your-dataset-id'
+table_id = 'your-table-id'
+
 if __name__ == '__main__':
 
     logger = log_config()
     
     json_list = extract_data(bucket_name, file_name)
 
-    create_bq_schema(json_list)
+    bq_schema, source_field_paths = create_bq_schema(json_list)
